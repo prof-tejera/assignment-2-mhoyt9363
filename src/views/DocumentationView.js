07c319dcef89f90d864da1128e997ea49e90dd60
Mike Hoyt <mhoyt9363@gmail.com>
2022-11-17 12:00:19 -0500

    documented timers

--- src/views/DocumentationView.js
+++ src/views/DocumentationView.js
@@ -4,6 +4,10 @@ import DocumentComponent from "../components/documentation/DocumentComponent";
 
 import Loading from "../components/generic/Loading";
 import ElapsedTime from "../components/generic/ElapsedTime";
+import Stopwatch from "../components/timers/Stopwatch";
+import Countdown from "../components/timers/Countdown";
+import XY from "../components/timers/XY";
+import Tabata from "../components/timers/Tabata";
 
 const Container = styled.div`
   display: flex;
@@ -23,6 +27,96 @@ const Documentation = () => {
     <Container>
       <div>
         <Title>Documentation</Title>
+        <DocumentComponent
+          title="Stopwatch Timer"
+          component={<Stopwatch duration={10} progress={0} />}
+          propDocs={[
+            {
+              prop: "duration",
+              description: "The # of seconds to complete the timer",
+              type: "number",
+              defaultValue: 10,
+            },
+            {
+              prop: "progress",
+              description: "The # of seconds elapsed",
+              type: "number",
+              defaultValue: 0,
+            }
+          ]}
+        />
+        <DocumentComponent
+          title="Countdown Timer"
+          component={<Countdown duration={10} progress={0} />}
+          propDocs={[
+            {
+              prop: "duration",
+              description: "The # of seconds to complete the timer",
+              type: "number",
+              defaultValue: 10,
+            },
+            {
+              prop: "progress",
+              description: "The # of seconds elapsed",
+              type: "number",
+              defaultValue: 0,
+            }
+          ]}
+        />
+        <DocumentComponent
+          title="XY Timer"
+          component={<XY rounds={3} work={10} progress={0} />}
+          propDocs={[
+            {
+              prop: "rounds",
+              description: "The # of rounds (X) to complete",
+              type: "number",
+              defaultValue: 3,
+            },
+            {
+              prop: "work",
+              description: "The # of seconds (Y) for each round",
+              type: "number",
+              defaultValue: 10,
+            },
+            {
+              prop: "progress",
+              description: "The total number of seconds completed",
+              type: "number",
+              defaultValue: 0,
+            }
+          ]}
+        />
+        <DocumentComponent
+          title="Tabata Timer"
+          component={<Tabata rounds={3} work={10} rest={5} progress={0} />}
+          propDocs={[
+            {
+              prop: "rounds",
+              description: "The # of rounds to complete",
+              type: "number",
+              defaultValue: 3,
+            },
+            {
+              prop: "work",
+              description: "The # of seconds to exercise each round",
+              type: "number",
+              defaultValue: 10,
+            },
+            {
+              prop: "rest",
+              description: "The # of seconds to rest each round",
+              type: "number",
+              defaultValue: 5,
+            },
+            {
+              prop: "progress",
+              description: "The total number of seconds completed",
+              type: "number",
+              defaultValue: 0,
+            }
+          ]}
+        />
         <DocumentComponent
           title="Displaying Time"
           component={<ElapsedTime seconds={65} />}
